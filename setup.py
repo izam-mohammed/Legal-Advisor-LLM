a08c99ebbe6446e2fb55d73f529606f3a6d7683c
Izam Mohammed <izamdeveloper1@gmail.com>
2024-03-03 16:46:43 +0530

    added the setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Legal Chatbot",
+    version="0.0.1",
+    author="Izam Mohammed",
+    author_email="izamdeveloper1@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
